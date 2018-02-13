140575f5a178cc348b9ccd4de4c86666fb9efd23
Joel Nieto <joelnietotec@outlook.com>
2018-02-13 09:21:11 -0500

    user message

--- src/app/dashboard/users/user-form/user-form.component.ts
+++ src/app/dashboard/users/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Validators, Form, FormControl, FormGroup, FormBuilder, EmailValidator } from '@angular/forms';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 import { User } from '../../../shared/models/users.model';
 import { UserService } from '../../../shared/services/users.service';
@@ -10,34 +12,31 @@ import { UserService } from '../../../shared/services/users.service';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
-
   _userForm: FormGroup;
   _user: User = {};
 
-  constructor(
-    private _fb: FormBuilder,
-    private _userServ: UserService
-  ) { }
+  constructor(private _fb: FormBuilder, private _userServ: UserService, private _router: Router, private toastr: ToastsManager) {}
 
   ngOnInit() {
-    this._userForm = new FormGroup({
-      UserName: new FormControl('', [Validators.required, Validators.minLength(5)]),
-      Email: new FormControl('', Validators.required),
-      Password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      PasswordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
-    }, this.passwordConfirm);
+    this._userForm = new FormGroup(
+      {
+        UserName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+        Email: new FormControl('', Validators.required),
+        Password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        PasswordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
+      },
+      this.passwordConfirm
+    );
   }
 
   saveUser(model: User) {
-    this._userServ.createUser(model)
-      .subscribe(data => {
-        console.log(data);
-      });
+    this._userServ.createUser(model).subscribe(data => {
+      this.toastr.success('Usuario creado exitosamente', model.UserName);
+      this._router.navigate(['../']);
+    });
   }
 
   passwordConfirm(g: FormGroup) {
-    return g.get('Password').value === g.get('PasswordConfirm').value
-      ? null : { 'mismatch': true };
+    return g.get('Password').value === g.get('PasswordConfirm').value ? null : { mismatch: true };
   }
-
 }
