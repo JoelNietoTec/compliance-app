81d9243eb131e50aa24d68bd2c6ccc0fc175cc35
Joel Nieto <joelnietotec@outlook.com>
2018-02-28 19:35:19 -0500

    Cronogramas fix

--- src/app/shared/components/chart-card/chart-card.component.ts
+++ src/app/shared/components/chart-card/chart-card.component.ts
@@ -6,25 +6,29 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./chart-card.component.css']
 })
 export class ChartCardComponent implements OnInit {
-  @Input() items: Array<any>;
+  @Input() items: any[];
   @Input() label: string;
   @Input() data: string;
   @Input() options: any;
   @Input() type?: string;
-  @Input() colors?: Array<string>;
+  @Input() colors?: string[];
+  @Input() title?: string;
 
   _labels: Array<string>;
   _values: Array<number>;
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this._labels = [];
     this._values = [];
+
+    if (!this.title) {
+      this.title = this.options.title.text;
+    }
 
     this.items.forEach(element => {
       this._labels.push(element[this.label]);
       this._values.push(element[this.data]);
     });
   }
-
 }
