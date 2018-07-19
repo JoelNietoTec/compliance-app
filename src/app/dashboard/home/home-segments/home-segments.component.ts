f6de27f9d21da19644ff8a0935cef8a042347680
Joel Nieto <joel-nieto15@outlook.com>
2018-07-19 07:26:37 -0500

    segments 2

--- src/app/dashboard/home/home-segments/home-segments.component.ts
+++ src/app/dashboard/home/home-segments/home-segments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ParamsService } from '../../../shared/services/params.service';
 import { Observable } from 'rxjs';
 import { Param } from '../../../shared/models/params.model';
+import { UtilService } from '../../../shared/services/util.service';
 
 @Component({
   selector: 'app-home-segments',
@@ -9,13 +10,17 @@ import { Param } from '../../../shared/models/params.model';
   styleUrls: ['./home-segments.component.css']
 })
 export class HomeSegmentsComponent implements OnInit {
-
   _params: Observable<Param[]>;
 
-  constructor(private _paramService: ParamsService) { }
+  constructor(
+    private _paramService: ParamsService,
+    private _util: UtilService
+  ) {}
 
   ngOnInit() {
-    this._params = this._paramService.getParams();
+    this._params = this._paramService.getParams().map(data => {
+      data = this._util.sortBy(data, 'name');
+      return data;
+    });
   }
-
 }
